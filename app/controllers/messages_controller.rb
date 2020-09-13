f4a1d60e2470bad8e83275d4d7a4d56935645b3b
hemp.salt <ec2-user@ip-172-31-39-242.ec2.internal>
2020-09-13 10:39:12 +0000

    add pagination

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
   before_action :set_message, only: [:show, :edit, :update, :destroy]
   
   def index
-    @messages = Message.all
+    @messages = Message.all.page(params[:page]).per(10)
   end
 
   def show
@@ -52,6 +52,6 @@ class MessagesController < ApplicationController
 
   # Strong Parameter
   def message_params
-    params.require(:message).permit(:content)
+    params.require(:message).permit(:content, :title)
   end
 end
